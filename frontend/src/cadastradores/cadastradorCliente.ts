bc945896171823459a555175313ec1af3ff0d297
Katiane <katiane.soares@fatec.sp.gov.br>
2023-11-24 18:19:19 -0300

    inclui a funcionalidade cadastrar cliente

--- frontend/src/cadastradores/cadastradorCliente.ts
+++ frontend/src/cadastradores/cadastradorCliente.ts
@@ -2,15 +2,25 @@ import { URI } from "../enuns/uri";
 import Cadastrador from "./cadastrador";
 
 class CadastradorCliente implements Cadastrador {
-    cadastrar(objeto: Object): void {
-        fetch(URI.CADASTRAR_CLIENTE, {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify(objeto)
-        })
-    }
+    async cadastrar(objeto: Object): Promise<void> {
+        try {
+            const response = await fetch(URI.CADASTRAR_CLIENTE, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(objeto)
+            });
 
+            if (!response.ok) {
+                throw new Error(`HTTP error! status: ${response.status}`);
+            }
+
+            const data = await response.json();
+            console.log(data);
+        } catch (error) {
+            console.error('There was a problem with the fetch operation: ', error);
+        }
+    }
 }
 export default CadastradorCliente
--- frontend/src/unidadeSJC/cadastroSJC.tsx
+++ frontend/src/unidadeSJC/cadastroSJC.tsx
@@ -1,155 +1,134 @@
-import React, { useState, ChangeEvent, FormEvent } from 'react';
+import React, { useState } from 'react';
+import axios from 'axios';
+import { Cliente, Endereco } from '../cliente';
 import styles from '../estilos/styles.module.css';
 import CadastradorCliente from '../cadastradores/cadastradorCliente';
-
-
+import { useHistory } from 'react-router-dom';
 
 function CadastroSJC() {
-    const [state, setState] = useState({
+    
+    const [clienteCadastrar, setClienteCadastrar] = useState<Cliente | null>(null);
+    const [clientes, setClientes] = useState<Cliente[]>([]);
+    const [telefone, setTelefone] = useState({ddd: '', numero: ''});
+    const [novoCliente, setNovoCliente] = useState<Cliente>({
+        id: '',
         nome: '',
-        nomeSocial: '',
-        cpf: '',
-        rgs: '',
-        genero: '',
-        telefones: '',
-        produto: '',
-        valorProduto: 0,
-        servico: '',
-        valorServico: 0
+        sobreNome: '',
+        endereco: {
+        cidade: '',
+        estado: '',
+        bairro: '',
+        rua: '',
+        numero: '',
+        codigoPostal: '',
+        informacoesAdicionais: '',
+        },
+        telefones: [{ddd: '', numero: ''}],
     });
 
-    function handleInputChange(event: ChangeEvent<HTMLInputElement | HTMLSelectElement>) {
-        const target = event.target;
-        const value = target.type === 'number' ? Number(target.value) : target.value;
-        const name = target.name;
-    
-        setState(prevState => ({ ...prevState, [name]: value }));
-    }
+const cadastradorCliente = new CadastradorCliente();    
 
-    const cadastradorCliente = new CadastradorCliente();
+const history = useHistory();
 
-    function handleSubmitCliente(event: FormEvent) {
+const handleNomeChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setNovoCliente(prevState => ({...prevState, nome: event.target.value}));
+  };
+  
+  const handleSobreNomeChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setNovoCliente(prevState => ({...prevState, sobreNome: event.target.value}));
+  };
+  
+  const handleEnderecoChange = (field: string, event: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
+    setNovoCliente(prevState => {
+      const newEndereco = {...prevState.endereco};
+      newEndereco[field as keyof Endereco] = event.target.value;
+      return {...prevState, endereco: newEndereco};
+    });
+  };
+  
+  function handlePhoneChange(event: React.ChangeEvent<HTMLInputElement>) {
+    const value = event.target.value.replace(/\D/g, ''); 
+    setNovoCliente(prevState => ({...prevState, telefones: [{ddd: '', numero: value}]}));
+  }
+  
+  // ...
+  
+  
+
+
+
+  const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
-    cadastradorCliente.cadastrar(state);
-    // ...
-    }
+    await cadastradorCliente.cadastrar(novoCliente);
+    history.push('/clienteSJC');
+};
+function formatPhoneNumber(ddd: string, numero: string) {
+    return `(${ddd}) ${numero.substring(0, 4)}-${numero.substring(4)}`;
+  }
 
-
-    function handleSubmitProduto(event: FormEvent) {
-        event.preventDefault();
-        console.log(state);
-        setState(prevState => ({
-            ...prevState,
-            produto: '',
-            valorProduto: 0
-        }));
-    }
-    
-    function handleSubmitServico(event: FormEvent) {
-        event.preventDefault();
-        console.log(state);
-        setState(prevState => ({
-            ...prevState,
-            servico: '',
-            valorServico: 0
-        }));
-    }
-    
     return (
-        <>
-        <div className={styles['container-lista']}>
-            <div className={styles['wrap-cadastro']}>
-                <div className={styles['titulo-cadastro']}>
-                    <h1>Cadastro de Clientes</h1>
-                </div>
-                <form  onSubmit={handleSubmitCliente}>
-                    <div className={styles['form-group']}>
-                        <label>Nome:</label>
-                        <input type="text" className={styles['full-width']} name="nome" value={state.nome} onChange={handleInputChange} required />
-                    </div>
-                    <div className={styles['form-group']}>
-                        <label>Nome Social:</label>
-                        <input type="text" className={styles['full-width']} name="nomeSocial" value={state.nomeSocial} onChange={handleInputChange} />
-                    </div>                        
-                    <div className={styles['form-group'] + ' ' + styles['flex-container']}>
-                        <div className={styles['half']}>
-                            <label>CPF:</label>
-                            <input type="text" name="cpf" value={state.cpf} onChange={handleInputChange} required />
-                        </div>
-                        <div className={styles['half']}>
-                            <label>RG:</label>
-                            <input type="text" name="rgs" value={state.rgs} onChange={handleInputChange} />
-                        </div>
-                    </div>
-                    <div className={styles['form-group'] + ' ' + styles['flex-container']}>
-                    <div className={styles['half']}>
-                        <label>Gênero:</label>
-                        <select name="genero" value={state.genero} onChange={handleInputChange} required>
-                            <option value="">Selecione...</option>
-                            <option value="masculino">Masculino</option>
-                            <option value="feminino">Feminino</option>
-                        </select>
-                    </div>
-                    <div className={styles['half']}>
-                        <label>Telefones:</label>
-                        <input type="text" name="telefones" value={state.telefones} onChange={handleInputChange} />
-                    </div>
-                </div>
-                    <div className={styles['form-group']}>
-                        <input type="submit" value="Enviar" />
-                    </div>
-                </form>
+    <div className={styles['container-lista']}>
+        <div className={styles['wrap-cadastro']}>
+            <div className={styles['titulo-cadastro']}>
+                <h1>Cadastro de Clientes</h1>
             </div>
-         
-        
-        
-            <div className={styles['wrap-cadastro2']}>
-                <div className={styles['titulo-cadastro']}>
-                    <h1>Cadastro de Produtos </h1>
+            <form onSubmit={handleSubmit}>
+                <div className={styles['form-group']}>
+                    <label>Nome:</label>
+                    <input type="text" className={styles['full-width']} name="nome" value={novoCliente.nome} onChange={handleNomeChange} required />
                 </div>
-                <form onSubmit={handleSubmitProduto}>
-                    <div className={styles['form-group'] + ' ' + styles['flex-container']}>
-                        <div className={styles['half']}>
-                            <label>Produto:</label>
-                            <input type="text" name="produto" value={state.produto} onChange={handleInputChange} required />
-                        </div>
-                        <div className={styles['half']}>
-                            <label>Valor:</label>
-                            <input type="number" name="valorProduto" value={state.valorProduto} onChange={handleInputChange} required />
-                        </div>
-                    </div>
-                    <div className={styles['form-group']}>
-                        <input type="submit" value="Enviar" />
-                    </div>
-                </form>
-            </div>
-         
-
-        
-            <div className={styles['wrap-cadastro2']}>
-                <div className={styles['titulo-cadastro']}>
-                    <h1>Cadastro de Serviços </h1>
+                <div className={styles['form-group']}>
+                    <label>Sobrenome:</label>
+                    <input type="text" className={styles['full-width']} name="sobreNome" value={novoCliente.sobreNome} onChange={handleSobreNomeChange} />
+                </div>                
+              {/* <div className={styles['form-group'] + ' ' + styles['flex-container']}>
+                <div className={styles['half']}>
+                  <label>CPF:</label>
+                  <input type="text" name="cpf" value={novoCliente.cpf} onChange={handleInputChange} required />
                 </div>
-                <form onSubmit={handleSubmitServico}>
-                    <div className={styles['form-group'] + ' ' + styles['flex-container']}>
-                        <div className={styles['half']}>
-                            <label>Serviço:</label>
-                            <input type="text" name="servico" value={state.servico} onChange={handleInputChange} required />
-                        </div>
-                        <div className={styles['half']}>
-                            <label>Valor:</label>
-                            <input type="number" name="valorServico" value={state.valorServico} onChange={handleInputChange} required />
-                        </div>
-                    </div>
-                    <div className={styles['form-group']}>
-                        <input type="submit" value="Enviar" />
-                    </div>
-                </form>
-            </div>
-        </div> 
-    </>
                 
-    );
-    }
+                <div className={styles['half']}>
+                  <label>Gênero:</label>
+                  <select name="genero" value={novoCliente.genero} onChange={handleInputChange} required>
+                    <option value="">Selecione...</option>
+                    <option value="masculino">Masculino</option>
+                    <option value="feminino">Feminino</option>
+                  </select>
+                </div>
+              </div> */}
+              <div className={styles['form-group'] + ' ' + styles['flex-container']}>
+                
+                <div className={styles['half']}>
+                    <label>Telefone:</label>
+                    <input type="text" className={styles['full-width']} name="telefone" value={novoCliente.telefones[0]?.numero} onChange={handlePhoneChange} required />
+                </div>
+                </div>
+                <div className={styles['form-group']}>
+                    <label>Endereço:</label>
+                    <input type="text" className={styles['full-width']} name="rua" value={novoCliente.endereco.rua} onChange={(event) => handleEnderecoChange('rua', event)} required />
+                </div>
+                <div className={styles['form-group'] + ' ' + styles['flex-container']}>
+                    <div className={styles['half']}>
+                        <label>Cidade:</label>
+                        <input type="text" className={styles['full-width']} name="cidade" value={novoCliente.endereco.cidade} onChange={(event) => handleEnderecoChange('cidade', event)} required />
+                    </div>
+                    <div className={styles['half']}>
+                        <label>Estado:</label>
+                        <input type="text" className={styles['full-width']} name="estado" value={novoCliente.endereco.estado} onChange={(event) => handleEnderecoChange('estado', event)} required />
+                    </div>
+                </div>
+                <div className={styles['form-group']}>
+                    <button type="submit" className={styles['btn-submit']}>Cadastrar</button>
+                </div>
+                    </form>
+                </div>
+            </div>
+            );
+        }
+    
+    
 
 export default CadastroSJC;
+
+
+      
